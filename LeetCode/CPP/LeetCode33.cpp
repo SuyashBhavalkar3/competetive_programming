23244da61a115883b41778e6a16818749900c72a
SuyashBhavalkar3 <suyashbhavalkar82@gmail.com>
2026-06-10 06:58:15 +0530

    LeetCode 33 in CPP

--- LeetCode/CPP/LeetCode33.cpp
+++ LeetCode/CPP/LeetCode33.cpp
@@ -7,55 +7,40 @@
 
 // Look For Reference In the Book On Pg No 23
 
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int binarySearchInRotatedArray(vector<int>& nums, int target) {
+int searchInRotatedArray(vector<int>& nums, int target){
     int low = 0;
     int high = nums.size()-1;
-    while (low<=high){
+    while(low<=high){
         int mid = low + (high-low)/2;
-        if(nums[mid] == target){
+        if(nums[mid]==target){
             return mid;
         }
-        if(nums[low]<=nums[mid]){
-            if((nums[low]<=target)&&(target<nums[high]))
-            high = mid-1;
-            else
-            low = mid+1;
+        if(nums[low]<nums[mid]){
+            if(nums[low]<=target && target<nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
         else{
-            if((nums[mid]<target)&&(target<=nums[high]))
-            low = mid+1;
-            else
-            high = mid-1;
+            if(nums[mid]<target && target<=nums[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
     }
     return -1;
 };
 
-int main() {
-    int limit;
-    cout << "Enter Limit Of Array : ";
-    cin >> limit;
-
-    vector<int> arr(limit);
-    cout << "Enter Elements : ";
-    for (int i = 0; i < limit; i++) {
-        cin >> arr[i];
-    }
-
-    int target;
-    cout << "Enter Target Element : ";
-    cin >> target;
-
-    int res = binarySearchInRotatedArray(arr, target);
-    if (res != -1) {
-        cout << "Target Element Found At Index " << res << endl;
-    } else {
-        cout << "Target Element Not Found." << endl;
-    }
-
-    return 0;
+int main(){
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int index = searchInRotatedArray(nums, 0);
+    cout<<"The Element Is Present At Index : "<<index<<endl;
 }
