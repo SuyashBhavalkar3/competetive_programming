8474fe7599f3da99c68079f1fe871a66893ad9eb
SuyashBhavalkar3 <suyashbhavalkar82@gmail.com>
2026-09-01 19:51:36 +0530

    Solved Celebrity Problem in Cpp from Strivers problem

--- Striver/CPP/celebrity_problem.cpp
+++ Striver/CPP/celebrity_problem.cpp
@@ -1,44 +1,40 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int findCelebrity(vector<vector<int>>& matrix){
-    int n = matrix.size();
-    int top = 0;
-    int down = n-1;
-    while(top<down){
-        if(matrix[top][down]==1){
-            top++;
-        }
-        else if(matrix[down][top]==1){
-            down--;
-        }
-        else{
-            top++;
-            down--;
-        }
+int findCelebrity(vector<vector<int>> &matrix) {
+  int n = matrix.size();
+  int top = 0;
+  int down = n - 1;
+  while (top < down) {
+    if (matrix[top][down] == 1) {
+      top++;
+    } else if (matrix[down][top] == 1) {
+      down--;
+    } else {
+      top++;
+      down--;
     }
-    if(top>down){
-        return -1;
+  }
+  if (top > down) {
+    return -1;
+  }
+  for (int i = 0; i < n; i++) {
+    if (i == top) {
+      continue;
     }
-    for(int i=0;i<n;i++){
-        if(i==top){
-            continue;
-        }
-        if(matrix[top][i]==1 || matrix[i][top]==0){
-            return -1;
-        }
+    if (matrix[top][i] == 1 || matrix[i][top] == 0) {
+      return -1;
     }
-    return top;
+  }
+  return top;
 }
 
-int main(){
-    vector<vector<int>> matrix = {{0, 1, 1, 0}, 
-                                  {0, 0, 0, 0}, 
-                                  {1, 1, 0, 0}, 
-                                  {0, 1, 1, 0}};
-    int celebrity = findCelebrity(matrix);
-    cout<<"The Celebrity Is At Index At : "<<celebrity<<endl;
-    return 0;
+int main() {
+  vector<vector<int>> matrix = {
+      {0, 1, 1, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}};
+  int celebrity = findCelebrity(matrix);
+  cout << "The Celebrity Is At Index At : " << celebrity << endl;
+  return 0;
 }
